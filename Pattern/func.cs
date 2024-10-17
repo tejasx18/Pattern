335283150c39683120221431e4cc5cad92a8f704
Tejas <ptejas003@gmail.com>
2024-10-17 15:38:11 +0530

    modified class func

--- Pattern/func.cs
+++ Pattern/func.cs
@@ -8,10 +8,17 @@ namespace Pattern
 {
     internal class func
     {
-        static int sample(int input) { 
-            if(input == 832) return 594;
-            if (input == 51) return 36;
-            return 0;
+        static int sample(int input) {
+            int temp = input;
+            int reverse = 0;
+            while (temp > 0) { 
+                int r = temp%10;
+                reverse= reverse*10+r;
+                temp = temp/10;
+            }
+            return input-reverse;
+            //double result = (input * 71.4) / 100;
+            //return (int)result;
         }
         static void Main(string[] args)
         {
